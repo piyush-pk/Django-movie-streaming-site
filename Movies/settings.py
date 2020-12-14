1afbd7548a6c888ac34849531b7cb18d2c4e4d1a
Piyush Khandelwal <55063039+piyush-pk@users.noreply.github.com>
2020-12-15 00:12:19 +0530

    Update settings.py

--- Movies/settings.py
+++ Movies/settings.py
@@ -10,9 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import bootstrap_admin
+import django_heroku
 from pathlib import Path
 from django.conf import global_settings
 
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
